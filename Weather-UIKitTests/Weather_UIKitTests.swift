7a0f8ea2bad2a0d294c60671a5292dd542f167e1
Сухорослов Виталий <v.sukhoroslov@l-tech.ru>
2024-11-03 11:13:48 +0300

    - setting project

---
+++ Weather-UIKitTests/Weather_UIKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_UIKitTests.swift
+//  Weather-UIKitTests
+//
+//  Created by Виталий Сухорослов on 02.11.2024.
+//
+
+import Testing
+@testable import Weather_UIKit
+
+struct Weather_UIKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
